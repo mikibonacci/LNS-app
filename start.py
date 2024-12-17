80479a26f45099954534005aabb13acfe362f131
Miki Bonacci <miki.bonacci@psi.ch>
2024-12-17 13:13:46 +0000

    adding title, and fancy buttons

--- start.py
+++ start.py
@@ -2,33 +2,60 @@ import ipywidgets as ipw
 
 
 def get_start_widget(appbase, jupbase, notebase):  # noqa: ARG001
+    return ipw.HTML(f"""
+        <div class="app-container">
+            <h1 
+            style="text-align:center; 
+            font-size:50px;">
+            Multiplexing Spectrometer CAMEA </h1>
+            <div class="features">
+                <a
+                    class="feature"
+                    href="{appbase}/plot.ipynb"
+                    target="_blank">
+                    <i 
+                        class="fa fa-bar-chart feature-logo" 
+                        style="font-size:40px;" 
+                        alt="Plot">
+                    </i>
+                    <div class="feature-label">Plot</div>
+                </a>
+                <a
+                    class="feature"
+                    href="{appbase}/mjolnir.ipynb"
+                    target="_blank">
+                    <i 
+                        class="fa fa-laptop feature-logo" 
+                        style="font-size:40px;" 
+                        alt="Mjolnir analysis">
+                    </i>
+                    <div class="feature-label">Mjolnir analysis</div>
+                </a>
+            </div>
+        </div>
+    """)
+    
+    
+    
     return ipw.HTML(
         f"""
-        <table>
-        <tr>
-        <th style="text-align:center">Options</th>
-        </tr>
-        <tr>
-        <td style="text-align:center">
-            <a href="{appbase}/mjolnir.ipynb" target="_blank">
-                <img src="https://example.com/mjolnir_logo.jpg" height="60px" width="60px"><br>
-                Mjolnir
-            </a>
-        </td>
-        <td style="text-align:center">
-            <a href="{appbase}/plot.ipynb" target="_blank">
-                <img src="https://example.com/plot_logo.jpg" height="60px" width="60px"><br>
-                Plot
-            </a>
-        </td>
-        </tr>
-        </table>
+        <div class="app-container">
+            <h1 style="text-align:center; font-size:50px;">LNS Apps</h1>
+            <div class="features">
+                <a 
+                    class="feature"
+                    href="{appbase}/mjolnir.ipynb" 
+                    target="_blank">
+                    <i 
+                        class="fa fa-laptop feature-logo" 
+                        style="font-size:40px;" 
+                        alt="Mjolnir analysis"
+                    </i>
+                    <div class="feature-label">Mjolnir analysis</div>
+                </a>
+            </div>
+        </div>
         """
     )
+
     
-    #<div align="center">
-    #    <a href="{appbase}/qe.ipynb" target="_blank">
-    #        <img src="https://gitlab.com/QEF/q-e/raw/develop/logo.jpg" height="120px" width=243px">
-    #    </a>
-    #</div>"""
-    #)
