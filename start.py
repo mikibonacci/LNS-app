c823f95ad9a3f3989e4da5e3e1aa814a1ad5f113
Edan Bainglass <edan.bainglass@gmail.com>
2026-07-12 13:49:18 +0200

    Rephrase button titles and add descriptions

--- start.py
+++ start.py
@@ -12,35 +12,41 @@ def get_start_widget(appbase, jupbase, notebase):  # noqa: ARG001
                 <a
                     class="feature"
                     href="{appbase}/plot.ipynb"
-                    target="_blank">
+                    target="_blank"
+                    title="Plot any 2D data (uploaded as CSV)"
+                >
                     <i
                         class="fa fa-bar-chart feature-logo"
                         style="font-size:40px;"
                         alt="Plot">
                     </i>
-                    <div class="feature-label">Plot GUI</div>
+                    <div class="feature-label">Plot 2D data</div>
                 </a>
                 <a
                     class="feature"
                     href="{appbase}/proposal_history.ipynb"
-                    target="_blank">
+                    target="_blank"
+                    title="Select and analyze data from the CAMEA instrument (using MJOLNIR)"
+                >
                     <i
                         class="fa fa-folder-open feature-logo"
                         style="font-size:40px;"
                         alt="Mjolnir analysis">
                     </i>
-                    <div class="feature-label">Analysis of CAMEA data (MJOLNIR)</div>
+                    <div class="feature-label">Analyze CAMEA data (MJOLNIR)</div>
                 </a>
                 <a
                     class="feature"
                     href="{appbase}/image-viewer-demo-ICON.ipynb"
-                    target="_blank">
+                    target="_blank"
+                    title="Select and analyze data from the ICON imaging instrument"
+                >
                     <i
                         class="fa fa-folder-open feature-logo"
                         style="font-size:40px;"
                         alt="Mjolnir analysis">
                     </i>
-                    <div class="feature-label">Analysis of ICON data</div>
+                    <div class="feature-label">Analyze ICON data</div>
                 </a>
             </div>
         </div>
